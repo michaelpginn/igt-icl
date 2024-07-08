5b3e2c4be7cd3c4d2df591d51d5243fbc9df674d
michaelpginn <milespergallon271@gmail.com>
2024-07-08 09:05:47 -0700

    Add remaining

--- experiments/run-all.sh
+++ experiments/run-all.sh
@@ -8,22 +8,22 @@
 #     done
 # done
 
-retriever="morpheme-recall"
-
-
-for lang in gitx1241 lezg1247 natu1246 uspa1245 
+for retriever in chrf word-recall word-precision max-word-coverage morpheme-recall
 do
-    # sh shots/glosslist/zeroshot-glosslist/run.sh 
+    for lang in gitx1241 lezg1247 natu1246 uspa1245 
+    do
+        # sh shots/glosslist/zeroshot-glosslist/run.sh 
 
-    for shots in 1 2 5 10 30 50
-    do   
-        for seed in 0 1 2
-        do 
-            if [ -f "retrieval/${retriever}/${retriever}-${shots}/${lang}.unseg.command-r-plus.${seed}.metrics.json" ]; then
-                echo "File already exists, skipping: retrieval/${retriever}/${retriever}-${shots}/${lang}.unseg.command-r-plus.${seed}.metrics.json"
-            else
-                sh "retrieval/${retriever}/${retriever}-$shots/run.sh" $lang $seed
-            fi
+        for shots in 1 2 5 10 30 50
+        do   
+            for seed in 0 1 2
+            do 
+                if [ -f "retrieval/${retriever}/${retriever}-${shots}/${lang}.unseg.command-r-plus.${seed}.metrics.json" ]; then
+                    echo "File already exists, skipping: retrieval/${retriever}/${retriever}-${shots}/${lang}.unseg.command-r-plus.${seed}.metrics.json"
+                else
+                    sh "retrieval/${retriever}/${retriever}-$shots/run.sh" $lang $seed
+                fi
+            done
         done
     done
 done
